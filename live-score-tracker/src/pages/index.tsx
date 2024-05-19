e16b1423b71ae1e5709845d72d007ccec6a04a9c
Brian Akhtar <kngf222@gmail.com>
2024-05-18 22:40:42 -0400

    Ensure project structure and index page

---
+++ live-score-tracker/src/pages/index.tsx
@@ -0,0 +1,12 @@
+// src/pages/index.tsx
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Live Score Tracker</h1>
+    </div>
+  );
+};
+
+export default Home;
